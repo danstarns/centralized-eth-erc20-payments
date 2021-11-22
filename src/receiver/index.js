502fb5fa105a3aa25c3a16f1a889075866ed199d
Daniel Starns <danielstarns@hotmail.com>
2021-11-22 16:29:20 +0000

    dev: add debug to receiver

--- src/receiver/index.js
+++ src/receiver/index.js
@@ -1,9 +1,14 @@
+const debug = require("../utils").debug("receiver");
 const deploy = require("./deploy");
 const deployed = require("./deployed");
 
 function listen() {
+  debug("Starting");
+
   deploy.listen();
   deployed.listen();
+
+  debug("Started");
 }
 
 module.exports = { listen };
